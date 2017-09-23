4099e6a66a3318f087969fa06e6c12706a928906
Andreas Reich <r_andreas2@web.de>
2017-09-23 21:19:16 +0200

    Added TrussShape data structure. Comes with a custom editor!

---
+++ Assets/TrussShape.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Single;
+using UnityEngine;
+using UnityEngine.VR;
+using UnityEngine.VR.WSA;
+
+public class TrussShape : MonoBehaviour
+{
+    [Serializable]
+    public struct Node
+    {
+        public Node(Vector2 pos)
+        {
+            Position = pos;
+            Fixed = false;
+        }
+        
+        public Vector2 Position;
+        public bool Fixed;
+    }
+
+    [Serializable]
+    public class Element
+    {
+        public float Thickness { get; set; } = 1.0f;
+        public float YoungModulus { get; set; } = 0.5f;
+
+        public int LeftNodeIdx { get; private set; }
+        public int RightNodeIdx { get; private set; }
+
+        public Element(int leftNodeIdx, int rightNodeIdx)
+        {
+            LeftNodeIdx = leftNodeIdx;
+            RightNodeIdx = rightNodeIdx;
+        }
+        
+        // Assume we are dealing with cylinders.
+        public float CrossSectionalArea => Thickness * Thickness * (float) Math.PI;
+
+        public float GetLength(IList<Node> nodes)
+        {
+            return (nodes[RightNodeIdx].Position - nodes[LeftNodeIdx].Position).magnitude;
+        }
+
+        public Matrix<float> GetStiffnessMatrix(IList<Node> nodes)
+        {
+            float k = CrossSectionalArea * YoungModulus / GetLength(nodes);
+
+            return DenseMatrix.OfArray(new[,] {{k, -k}, {-k, k}});
+        }
+    }
+    
+    public List<Node> Nodes = new List<Node>(
+        new[]
+        {
+            new Node {Position = Vector2.zero, Fixed = true},
+            new Node {Position = Vector2.right, Fixed = false}
+        });
+
+    public List<Element> Elements = new List<Element>(new[] { new Element(leftNodeIdx: 0, rightNodeIdx: 1) } );
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Elements
+        Gizmos.color = Color.black;
+        foreach (var element in Elements)
+        {
+            Gizmos.DrawLine(Nodes[element.LeftNodeIdx].Position.To3D() + transform.position, 
+                            Nodes[element.RightNodeIdx].Position.To3D() + transform.position);
+        }
+    }
+}
---
+++ Assets/TrussShapeEditor.cs
@@ -0,0 +1,77 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TrussShape))]
+class TrussShapeEditor : Editor
+{
+    private int selectedNode = 0;
+
+    public bool ShowLables { get; set; } = false;
+
+    protected virtual void OnSceneGUI()
+    {
+        TrussShape targetShape = (TrussShape)target;
+        if (targetShape == null)
+        {
+            return;
+        }
+
+        var targetShapePos = targetShape.transform.position;
+
+        GUIStyle style = new GUIStyle();
+        style.normal.textColor = Color.green;
+
+        // Adding nodes.
+        if (Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.Space)
+        {
+            var mousePosition = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition).origin;
+            EditorGUI.BeginChangeCheck();
+            targetShape.Nodes.Add(new TrussShape.Node(mousePosition.To2D()));
+            EditorGUI.EndChangeCheck();
+            Undo.RecordObject(targetShape, "Add truss node.");
+        }
+
+        // Nodes.
+        for (int i = 0; i < targetShape.Nodes.Count; ++i)
+        {
+            var node = targetShape.Nodes[i];
+            var pos = node.Position.To3D() + targetShapePos;
+
+            if (i == selectedNode)
+                Handles.color = Color.red;
+            else
+                Handles.color = Color.white;
+            
+            var buttonSize = HandleUtility.GetHandleSize(pos) * 0.08f;
+            Handles.CapFunction capfunc = Handles.SphereHandleCap;
+            if (node.Fixed)
+                capfunc = Handles.DotHandleCap;
+            if (Handles.Button(pos, Quaternion.identity, buttonSize, buttonSize, capfunc))
+            {
+                if (Event.current.shift)
+                {
+                    EditorGUI.BeginChangeCheck();
+                    targetShape.Elements.Add(new TrussShape.Element(selectedNode, i));
+                    EditorGUI.EndChangeCheck();
+                    Undo.RecordObject(targetShape, "Add truss element.");
+                }
+                selectedNode = i;
+            }
+                
+            
+            // Moving selected node.
+            if (i == selectedNode)
+            {
+                EditorGUI.BeginChangeCheck();
+                Vector3 newTargetPosition = Handles.PositionHandle(pos, Quaternion.identity);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RecordObject(targetShape, "Change TrussShape node position");
+                    newTargetPosition -= targetShapePos;
+                    node.Position = new Vector2(newTargetPosition.x, newTargetPosition.y);
+                    targetShape.Nodes[i] = node;
+                }
+            }
+        }
+    }
+}

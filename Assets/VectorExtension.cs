7586692619ce0f06f2788638520ed5b5ec151966
Andreas Reich <r_andreas2@web.de>
2017-09-23 21:10:26 +0200

    Added helper functions for 2d/3d vector use.

---
+++ Assets/VectorExtension.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public static class VectorExtension
+{
+    public static Vector3 To3D(this Vector2 v)
+    {
+        return new Vector3(v.x, v.y, 0.0f);
+    }
+    
+    public static Vector2 To2D(this Vector3 v)
+    {
+        return new Vector2(v.x, v.y);
+    }    
+}

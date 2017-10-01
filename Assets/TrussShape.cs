1d5a0ee29ba5c662e114ddeb13da429b04c33b92
Andreas Reich <r_andreas2@web.de>
2017-10-01 15:05:59 +0200

    Split up stiffness matrix computation in TrussShape Added some documentation. Added computation of mass and damping matrices.

--- Assets/TrussShape.cs
+++ Assets/TrussShape.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 using UnityEngine.VR;
 using UnityEngine.VR.WSA;
 
+// A 2D structure whose line elements can experience only axial stress
 public class TrussShape : MonoBehaviour
 {
     [Serializable]
@@ -20,6 +21,9 @@ public class TrussShape : MonoBehaviour
         }
         
         public Vector2 Position;
+        /// <summary>
+        /// If true, the node is constrained, i.e. it cannot be moved.
+        /// </summary>
         public bool Fixed;
     }
 
@@ -33,6 +37,11 @@ public class TrussShape : MonoBehaviour
 
         public float YoungModulus => YoungModulusGPa * 1000000000;
 
+        [Tooltip("kg per cubic meter. 1000 is water.")]
+        public float Density = 1000.0f;
+
+        public float DampingCoefficient = 50.0f;
+        
         public int LeftNodeIdx;
         public int RightNodeIdx;
 
@@ -41,19 +50,69 @@ public class TrussShape : MonoBehaviour
             LeftNodeIdx = leftNodeIdx;
             RightNodeIdx = rightNodeIdx;
         }
+
+        /// <summary>
+        /// Rotation matrix that is used to convert from 1D bar element to the 2D truss element
+        /// </summary>
+        /// <param name="dir">Normalixed from one node to the other.</param>
+        private Matrix<float> GetRotationMatrix(Vector2 dir)
+        {
+            float cosAngle = dir.x; // dot product with x axis divided by length.
+            float sinAngle = (float)Math.Sqrt(1.0f - cosAngle * cosAngle); // http://www.wolframalpha.com/input/?i=sin(cos%CB%86-1(x))
+            return DenseMatrix.OfArray(new[,] {{cosAngle, 0.0f}, {sinAngle, 0.0f}, {0.0f, cosAngle}, {0.0f, sinAngle}});
+        }
+
+        private void GetDirAndLength(IList<Node> nodes, out Vector2 dir, out float length)
+        {
+            dir = nodes[RightNodeIdx].Position - nodes[LeftNodeIdx].Position;
+            length = dir.magnitude;
+            dir /= length; 
+        }
         
         public Matrix<float> GetStiffnessMatrix(IList<Node> nodes)
         {
-            Vector2 dir = nodes[RightNodeIdx].Position - nodes[LeftNodeIdx].Position;
-            float length = dir.magnitude;
+            Vector2 dir;
+            float length;
+            GetDirAndLength(nodes, out dir, out length);
+            
+            // using a linear shape function
             float k = CrossSectionalArea * YoungModulus / length;
             var stiffnessMatrix1D = DenseMatrix.OfArray(new[,] {{k, -k}, {-k, k}});
 
-            float cosAngle = dir.x / length; // dot product with x axis divided by length.
-            float sinAngle = (float)Math.Sqrt(1.0f - cosAngle * cosAngle); // http://www.wolframalpha.com/input/?i=sin(cos%CB%86-1(x))
-            var rotationMatrix = DenseMatrix.OfArray(new[,] {{cosAngle, 0.0f}, {sinAngle, 0.0f}, {0.0f, cosAngle}, {0.0f, sinAngle}});
-
+            var rotationMatrix = GetRotationMatrix(dir);
             return rotationMatrix * stiffnessMatrix1D * rotationMatrix.Transpose();
+        }
+
+        /// <summary>
+        /// Mass matrix taking into account the shape functions.
+        /// </summary>
+        public Matrix<float> GetConsistentMassMatrix(IList<Node> nodes)
+        {
+            Vector2 dir;
+            float length;
+            GetDirAndLength(nodes, out dir, out length);
+
+            var m = Density * CrossSectionalArea * length / 6.0f;
+            var massMatrix1D =  DenseMatrix.OfArray(new[,] {{Density * 2, Density}, {Density, Density * 2}});
+            
+            var rotationMatrix = GetRotationMatrix(dir);
+            return rotationMatrix * massMatrix1D * rotationMatrix.Transpose();
+        }
+
+        /// <summary>
+        /// Daming matrix taking into account the shape function.
+        /// </summary>
+        /// <returns></returns>
+        public Matrix<float> GetConsistentDampingMatrix(IList<Node> nodes)
+        {
+            Vector2 dir;
+            float length;
+            GetDirAndLength(nodes, out dir, out length);
+            
+            var dampingMatrix1d =  DenseMatrix.OfArray(new[,] {{DampingCoefficient * 2 / 6.0f, DampingCoefficient / 6.0f}, 
+                                                               {DampingCoefficient / 6.0f, DampingCoefficient * 2 / 6.0f}});
+            var rotationMatrix = GetRotationMatrix(dir);
+            return rotationMatrix * dampingMatrix1d * rotationMatrix.Transpose();
         }
     }
 
@@ -74,34 +133,62 @@ public class TrussShape : MonoBehaviour
     public List<Element> Elements = new List<Element>(new[] { new Element(leftNodeIdx: 0, rightNodeIdx: 1) } );
 
     public Force[] Forces = new Force[0];
+
+    public bool Dynamic = true;
+    [Range(0.0f, 0.5f)]
+    public float NewMarkBeta = 0.25f;
+    [Range(0.0f, 1.0f)]
+    public float NewMarkGamma = 0.5f;
     
     private Vector<float> activeDisplacement = null;
-//    private Vector<float> elementStrains;
+    private Vector<float> speeds = null;
+    private Vector<float> accelerations = null;
+
+    void Start()
+    {
+        activeDisplacement = DenseVector.Create(Nodes.Count * 2, 0.0f);
+        speeds = DenseVector.Create(Nodes.Count * 2, 0.0f);
+        accelerations = DenseVector.Create(Nodes.Count * 2, 0.0f);
+    }
     
-    // Update is called once per frame
     void Update()
     {
         var stiffness = ComputeGlobalStiffnessMatrix();
         var force = ComputeForceVector();
-        activeDisplacement = stiffness.Solve(force);
-//
-//        for (int i = 0; i < Elements.Length; ++i)
-//        {
-//            var e = Elements[i];
-//            float elementLengthInv = 1.0f / e.GetLength(Nodes);
-//            elementStrains[i] = e.YoungModulus *
-//                                (-elementLengthInv * activeDisplacement[e.LeftNodeIdx] +
-//                                 elementLengthInv * activeDisplacement[e.RightNodeIdx]);
-//        }
+
+        if (Dynamic)
+        {
+            //float dT = Time.deltaTime;
+            //float dTSquared = Time.deltaTime * dTSquared;
+            // todo.
+        }
+        else
+        {
+            ApplyConstraints(stiffness);
+            stiffness.Solve(force, activeDisplacement);           
+        }
+    }
+
+    private Matrix<float> ComputeGlobalStiffnessMatrix()
+    {
+        return ComputeGlobalMatrix((e, n) => e.GetStiffnessMatrix(n));
+    }
+    private Matrix<float> ComputeGlobalMassMatrix()
+    {
+        return ComputeGlobalMatrix((e, n) => e.GetConsistentMassMatrix(n));
+    }
+    private Matrix<float> ComputeGlobalDampingMatrix()
+    {
+        return ComputeGlobalMatrix((e, n) => e.GetConsistentDampingMatrix(n));
     }
     
-    private Matrix<float> ComputeGlobalStiffnessMatrix()
+    private Matrix<float> ComputeGlobalMatrix(Func<Element, IList<Node>, Matrix<float>> getElementMatrixFunc)
     {
         Matrix<float> stiffnessMatrix = DenseMatrix.Create(Nodes.Count * 2, Nodes.Count * 2, 0.0f);
 
         foreach (var e in Elements)
         {
-            var stiffnessElementMatrix = e.GetStiffnessMatrix(Nodes);
+            var stiffnessElementMatrix = getElementMatrixFunc(e, Nodes);
 
             for (int r = 0; r < 4; ++r)
             {
@@ -114,20 +201,22 @@ public class TrussShape : MonoBehaviour
                 }
             }
         }
+        
+        return stiffnessMatrix;
+    }
 
-        // Constrained nodes.
+    private void ApplyConstraints(Matrix<float> globalMatrix)
+    {
         for (int nodeIdx = 0; nodeIdx < Nodes.Count; ++nodeIdx)
         {
             if (Nodes[nodeIdx].Fixed)
             {
-                stiffnessMatrix.ClearRows(nodeIdx*2, nodeIdx*2 + 1);
-                stiffnessMatrix.ClearColumns(nodeIdx*2, nodeIdx*2 + 1);
-                stiffnessMatrix[nodeIdx*2, nodeIdx*2] = 1.0f;
-                stiffnessMatrix[nodeIdx*2+1, nodeIdx*2+1] = 1.0f;
+                globalMatrix.ClearRows(nodeIdx*2, nodeIdx*2 + 1);
+                globalMatrix.ClearColumns(nodeIdx*2, nodeIdx*2 + 1);
+                globalMatrix[nodeIdx*2, nodeIdx*2] = 1.0f;
+                globalMatrix[nodeIdx*2+1, nodeIdx*2+1] = 1.0f;
             }
         }
-
-        return stiffnessMatrix;
     }
     
     private Vector<float> ComputeForceVector()
@@ -168,7 +257,6 @@ public class TrussShape : MonoBehaviour
     {
         EnsureValidNodes();
         EnsureValidElements();
-        //elementStrains = DenseVector.Create(Elements.Length, 0.0f);
     }
 
     private void EnsureValidNodes()
